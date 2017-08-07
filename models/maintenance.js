d27d6bd3a6cbaea7f6e6e96df90321cd55d329ac
MDC_Desktop <marya.crigler@att.net>
2017-08-06 20:54:22 -0500

    add application and maintenance html pages, models and schema

---
+++ models/maintenance.js
@@ -0,0 +1,78 @@
+module.exports = function(sequelize, Sequelize) {
+
+	var kdsApplication = sequelize.define('kdsApplication', {
+
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER
+		},
+
+		firstName: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
+
+		lastName: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
+
+		phone: {
+			type: Sequelize.STRING
+		},
+
+		email: {
+			type: Sequelize.STRING,
+			validate: {
+				isEmail: true
+			}
+		},
+
+		kdsNum: {
+			type: Sequelize.STRING
+		},
+
+		kdsAddr: {
+			type: Sequelize.STRING
+		},
+
+		kdsCity: {
+			type: Sequelize.STRING
+		},
+
+		kdsState: {
+			type: Sequelize.STRING
+		},
+
+		maintenanceCategory: {
+			type: Sequelize.STRING
+		},
+
+		maintenanceIssue: {
+			type: Sequelize.STRING
+		},
+
+		applItem: {
+			type: Sequelize.STRING
+		},
+
+		applMake: {
+			type: Sequelize.STRING
+		},
+
+		applModel: {
+			type: Sequelize.INTEGER
+		},
+
+		requestDt: {
+			type: Sequelize.DATE
+		},
+
+		{
+		  timestamps: false
+		}
+	});
+
+	return kdsApplication;
+};
