2c508e89d75e3a302418da0a0d2419568660e8be
Spencer-P-Brown <Spencer-P-Brown@github.com>
2017-08-08 19:20:32 -0500

    prevented sequelize from pluralizing table names in landlord.js, property.js, tenant.js

--- models/property.js
+++ models/property.js
@@ -66,10 +66,10 @@ module.exports = function(sequelize, DataTypes) {
         len:[1]
       }
     }
+  },{
+    freezeTableName: true,
 
-    // freezeTableName: true,
-
-    // tableName: "Property"
+    tableName: "Property"
 
   });
 
