04bef827834e4af72da782a78bae2869c1dc8999
Cody Childers <cody_k_childers@homedepot.com>
2017-08-12 17:17:34 -0500

    lern 2 inglish

--- controllers/controller.js
+++ controllers/controller.js
@@ -106,7 +106,7 @@ module.exports = function(app) {
 	// based on Flask-restless-
 	//  http://flask-restless.readthedocs.io/en/stable/quickstart.html
 
-	// Automated GET rotue for retrieving all items of a certain model
+	// Automated GET route for retrieving all items of a certain model
     app.get("/api/:model_name", function(req, res) {
 	  // TODO: take filters as GET params like '/api/:model_name?price=20' and insert into a 'where' clause
 	  let model_name = req.params.model_name;
@@ -117,7 +117,7 @@ module.exports = function(app) {
         });
     });
 
-	// Automated GET rotue for retrieving one item of a certain model
+	// Automated GET route for retrieving one item of a certain model
     app.get("/api/:model_name/:id", function(req, res) {
 	  let model_name = req.params.model_name;
   	  let sequelize_model_name = capitalize(model_name);
